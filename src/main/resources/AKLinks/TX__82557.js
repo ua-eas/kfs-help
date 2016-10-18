26f88ab147f6c228f83fdcbb1e238a60ff01abf4
kymberhorn <kymber@kuali.co>
2016-10-18 08:55:28 -0700

    Update to AR and PDP
    AR - FINP-724 - added business rule to CINV re: same account on multiple
    awards
    PDP - added note re: Ref Doc Type used when uploading payments via the
    Payment Spreadsheet Upload aka Participant Reserach payments

--- src/main/resources/AKLinks/TX__82557.js
+++
@@ -1,1 +0,0 @@
-d2hStoreMenuItems("TX__82557", [["WordDocuments/customerinformationsection.htm", "right", "Customer Information Section"],["WordDocuments/generaltab.htm", "right", "General Tab"]]);
---
+++ src/main/resources/AKLinks/TX__83053.js
@@ -0,0 +1,1 @@
+d2hStoreMenuItems("TX__83053", [["WordDocuments/postprocessing14.htm", "right", "Post Processing"],["WordDocuments/postprocessing16.htm", "right", "Post Processing"]]);
