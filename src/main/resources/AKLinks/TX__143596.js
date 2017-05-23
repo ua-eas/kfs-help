975d173a5f5936e2f1960019703b66bc6272620a
kymberhorn <kymber@kuali.co>
2017-05-23 13:22:03 -0700

    minor changes to FP

--- src/main/resources/AKLinks/TX__143596.js
+++
@@ -1,1 +0,0 @@
-d2hStoreMenuItems("TX__143596", [["WordDocuments/currencyandcoindetai.htm", "right", "Currency and Coin Detail Tab"],["WordDocuments/currencyandcoindetai1.htm", "right", "Currency and Coin Detail Tab"]]);
