868bf32a9b0534aec86e0baf8ae9d7723a55a608
kymberhorn <kymber@kuali.co>
2018-06-19 13:12:20 -0700

    FINBA-524 #2
    removed CGB data placeholders - last commit broke search

---
+++ src/main/resources/AKLinks/TX__16121.js
@@ -0,0 +1,1 @@
+d2hStoreMenuItems("TX__16121", [["WordDocuments/customerinformationsection.htm", "right", "Customer Information Section"],["WordDocuments/generaltab2.htm", "right", "General Tab"]]);
